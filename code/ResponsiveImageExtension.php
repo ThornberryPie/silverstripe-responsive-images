08fab24b1ab5aada4463d82e9c631cbd7fad2ca1
graemet <graeme.thornber@dna.co.nz>
2017-11-02 11:41:10 +1300

    REMOVE: picturefill

--- code/ResponsiveImageExtension.php
+++ code/ResponsiveImageExtension.php
@@ -86,7 +86,6 @@ class ResponsiveImageExtension extends \Extension
      */
     protected function createResponsiveSet($config, $defaultArgs, $set)
     {
-        Requirements::javascript(RESPONSIVE_IMAGES_DIR . '/javascript/picturefill/picturefill.min.js');
 
         if (!isset($config['arguments']) || !is_array($config['arguments'])) {
             throw new Exception("Responsive set $set does not have any arguments defined in its config.");
